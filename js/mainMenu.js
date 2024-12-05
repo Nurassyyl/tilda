4f813967e24cafd4e37847a06a4ee2fb48a6ece4
Nurassyl Abdildabek <abdildabekn@mail.ru>
2024-12-06 01:18:25 +0500

    menu

--- js/mainMenu.js
+++ js/mainMenu.js
@@ -1,15 +1,18 @@
 const menuBtnOpenMain = document.querySelector(".menu-btn-open");
 const menuBtnCloseMain = document.querySelector(".nav-close-btn");
-const menuMain = document.querySelector(".nav");
+const menuMain = document.querySelector(".nav-mobile");
 const menuDropBtn = document.querySelector(".menu-item-drop");
 const menuDropdowns = document.querySelector(".menu-list-dropdowns");
+const body = document.body;
 
 menuBtnOpenMain.addEventListener('click', () => {
   menuMain.style.transform = "translateX(0)";
+  body.classList.add('hidden');
 })
 
 menuBtnCloseMain.addEventListener('click', () => {
   menuMain.style.transform = "translateX(100%)";
+  body.classList.remove('hidden');
 })
 
 menuDropBtn.addEventListener('click', () => {
